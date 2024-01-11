744c53af12eda2dbcce718b20b09c14742b48682
oggiv <93593542+oggiv@users.noreply.github.com>
2024-01-11 23:58:33 +0100

    Added an image output
    Temporarily just copies the input pixel by pixel and saves it as output

--- test.py
+++ test.py
@@ -10,7 +10,17 @@ with open('ei.jpg', 'rb') as file:
 
 input_image = Image.open(input_image_buffer)
 
+width, height = input_image.size
 
+# create an empty output image with a black background
+output_image = Image.new('RGB', (width, height), 'black')
+
+# fill the pixels in the output, one by one, with the corresponding pixels from the input
+for x in range(width):
+    for y in range(height):
+        output_image.putpixel((x, y), input_image.getpixel((x, y)))
+
+output_image.save('output.jpg')
 
 # remember to close the file uwu
 input_image_buffer.close()
