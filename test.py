e7654e331a873cfb56f308499e204446ab667ea6
oggiv <93593542+oggiv@users.noreply.github.com>
2024-01-11 23:52:37 +0100

    Added opening and loading the test image

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 # test.py
 # convert jpg to polar image in a image buffer and then convert buffer to an output jpg
+
+from PIL import Image
+import io
+
+# read input image file into a PIL image
+with open('ei.jpg', 'rb') as file:
+    input_image_buffer = io.BytesIO(file.read())
+
+input_image = Image.open(input_image_buffer)
+
+
+
+# remember to close the file uwu
+input_image_buffer.close()
